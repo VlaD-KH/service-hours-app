3307c9c8202145f88c753824cb60e23f6d9a70b2
VlaD <vladhrapko@gmail.com>
2026-08-08 22:27:34 +0200

    fix: resolve table columns squishing and alignment issues on mobile by adding min-width to tables

---
+++ env.js
@@ -0,0 +1,11 @@
+// Сгенерировано автоматически через build-env.js
+window.ENV = {
+  FIREBASE_API_KEY: "AIzaSyBUKLLdkXmASmoq0b2YCbrmp-3U5FvVZ70",
+  FIREBASE_AUTH_DOMAIN: "vhs-zps.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "vhs-zps",
+  FIREBASE_STORAGE_BUCKET: "vhs-zps.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "122824920296",
+  FIREBASE_APP_ID: "1:122824920296:web:36d289606cdc2018b28d5d",
+  FIREBASE_MEASUREMENT_ID: "G-FVZ569X8ZZ",
+  ALLOWED_MASTERS: ["vladhrapko@gmail.com"]
+};
